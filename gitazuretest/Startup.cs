4b610c51b08ca84e0c2b58c88dd3a28e66329d9b
5umedh <sumedh.mashalkar@live.com>
2015-10-09 12:48:21 +0530

    first commit

---
+++ gitazuretest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gitazuretest.Startup))]
+namespace gitazuretest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
